a0bdf0e79775c5d8a618c633ee51918956357c12
My Name <tadzhibaevaelnura@gmail.com>
2023-06-25 01:10:51 +0300

    templateMethod

---
+++ src/behaviral/templateMethod/NewsPage.java
@@ -0,0 +1,9 @@
+package behaviral.templateMethod;
+
+public class NewsPage extends WebsiteTemplate{
+
+    @Override
+    public void showPageContent() {
+        System.out.println("Newspage");
+    }
+}
---
+++ src/behaviral/templateMethod/WelcomePage.java
@@ -0,0 +1,9 @@
+package behaviral.templateMethod;
+
+public class WelcomePage extends WebsiteTemplate{
+
+    @Override
+    public void showPageContent() {
+        System.out.println("Welcomepage" );
+    }
+}
